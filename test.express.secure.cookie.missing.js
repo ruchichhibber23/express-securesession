96728575fb2ee5429e4dcb3d0c19e2d193b64e6c
Rahul Upadhyaya <rahul.upadhyaya@3pillarglobal.com>
2016-10-14 12:28:28 +0530

    test

--- test.express.secure.cookie.missing.js
+++ test.express.secure.cookie.missing.js
@@ -5,23 +5,23 @@ var expSess = require("express-session");
 var app = express();
 
 var sess = {
-secret: 'keyboard cat',
-key: "sessionId",
-resave: true,
-saveUninitialized: true,
-cookie: {
-httpOnly: true
-}
+  secret: 'keyboard cat',
+  key: "sessionId",
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true
+  }
 };
 
 app.disable('x-powered-by');
 app.use(expSess(sess));
 
 app.get('/', function (req, res) {
-res.send('Hello World');
+  res.send('Hello World');
 });
 
 var server = app.listen(3000, function () {
-var port = server.address().port;
-console.log('Your app listening at http://localhost:%s', port);
+  var port = server.address().port;
+  console.log('Your app listening at http://localhost:%s', port);
 });
